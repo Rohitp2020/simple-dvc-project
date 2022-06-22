b383e5d213501bc6db14b64960a480a4232ec235
Rohitp2020 <rohitpunetha2001@gmail.com>
2022-06-22 13:19:03 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Rohitp2020",
+    description="A small package for dvc ml pipeline demo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Rohitp2020/simple-dvc-project.git",
+    author_email="rohitpunetha2001@gmail.com",
+    package_dir={"":"src"},
+    packages=find_packages(where="src"),licence="GNU",
+    python_requires=">=3.6",
+    install_requires=[
+        'dvc',
+        'dvc[gdrive]',
+        'dvc[s3]',
+        'pandas',
+        'scikit-learn'
+    ]
+)
